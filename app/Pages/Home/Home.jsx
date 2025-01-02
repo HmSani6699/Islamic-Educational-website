3d6a4b93f40956f706c474442a5408b5a5f45b6b
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-01-02 11:50:34 +0600

    start new islamic educational website

---
+++ app/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-white text-5xl font-bold text-center">
+        Welcome to Islamic Educational Website
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
