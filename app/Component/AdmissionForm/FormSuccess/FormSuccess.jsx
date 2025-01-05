72537c4190f0e0f16b3ebf3c11aed53991718c38
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-01-05 12:21:44 +0600

    added new success page

--- app/Component/AdmissionForm/FormSuccess/FormSuccess.jsx
+++ app/Component/AdmissionForm/FormSuccess/FormSuccess.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import success_icon from "../../../../public/svg/success-icon.svg";
+import { useNavigate } from "react-router";
 
 const FormSuccess = () => {
-  return <div>hallo success page</div>;
+  const navigat = useNavigate();
+  return (
+    <div>
+      <div className="px-[20px] w-[700px] mx-auto py-[30px] bg-white mt-[100px] flex flex-col items-center justify-center  rounded-[10px] border-t-[2px] border-[#008000]">
+        <img className="w-[70px]" src={success_icon} alt="" />
+        <h2 className="mt-[20px] text-[30px] font-semibold">ধন্যবাদ !</h2>
+        <h2 className="mt-[15px] text-[20px] ">
+          আপনার ফর্ম সফলভাবে জমা দেওয়া হয়েছে
+        </h2>
+        <button
+          onClick={() => navigat("/admin")}
+          className="flex items-center gap-[8px] bg-[#008000]  text-white px-[40px] py-[8px] rounded-[8px] mt-[30px]"
+        >
+          ড্যাশবোর্ড
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FormSuccess;
