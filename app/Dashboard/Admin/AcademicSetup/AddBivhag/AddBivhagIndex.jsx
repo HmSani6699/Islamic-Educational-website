b3dcc466264baace59774db158c4478ba7fd9d31
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-02-07 09:08:06 +0600

    added new bivhag part

--- app/Dashboard/Admin/AcademicSetup/AddBivhag/AddBivhagIndex.jsx
+++ app/Dashboard/Admin/AcademicSetup/AddBivhag/AddBivhagIndex.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { FaPlus } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaArrowLeft, FaPlus } from "react-icons/fa";
+import BivhagCreateUpdate from "./BivhagCreateUpdate";
+import BivhagList from "./BivhagList";
 
 const AddBivhagIndex = () => {
   const [openForm, setOpenForm] = useState("bivhagList");
@@ -7,17 +9,37 @@ const AddBivhagIndex = () => {
   return (
     <div>
       <div className="flex items-center justify-between  px-[20px] py-[10px] bg-white mt-[40px] rounded-t-[20px] border-t-[2px] border-[#008000]">
-        <h2 className="text-[20px] font-semibold">মোট বিভাগ (20)</h2>
-        <button
-          onClick={() => setOpenForm("bivhagCreateUpdate")}
-          className="flex items-center gap-[8px] bg-[#008000]  text-white px-[20px] py-[10px] rounded-[8px]"
-        >
-          <FaPlus />
-          নতুন বিভাগ
-        </button>
+        <div>
+          {openForm === "bivhagList" ? (
+            <h2 className="text-[20px] font-semibold">মোট বিভাগ (20)</h2>
+          ) : (
+            <h2
+              onClick={() => setOpenForm("bivhagList")}
+              className="text-[20px] font-semibold py-[6px] flex items-center gap-[10px] cursor-pointer"
+            >
+              <FaArrowLeft />
+              {openForm === "bivhagList" ? "নতুন বিভাগ" : "নতুন বিভাগ"}
+            </h2>
+          )}
+        </div>
+        {openForm === "bivhagList" && (
+          <button
+            onClick={() => setOpenForm("bivhagCreateUpdate")}
+            className="flex items-center gap-[8px] bg-[#008000]  text-white px-[20px] py-[10px] rounded-[8px]"
+          >
+            <FaPlus />
+            নতুন বিভাগ
+          </button>
+        )}
       </div>
-
-      <div></div>
+      {/* =====> Main content  <===== */}
+      <div>
+        {openForm === "bivhagCreateUpdate" ? (
+          <BivhagCreateUpdate />
+        ) : (
+          <BivhagList />
+        )}
+      </div>
     </div>
   );
 };
--- app/Dashboard/Admin/AcademicSetup/AddBivhag/BivhagCreateUpdate.jsx
+++ app/Dashboard/Admin/AcademicSetup/AddBivhag/BivhagCreateUpdate.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import InputField from "../../../../Component/InputField/InputField";
 
 const BivhagCreateUpdate = () => {
-  return <div>create update</div>;
+  return (
+    <div className="px-[20px] py-[10px] bg-white pt-[30px] mb-[50px] w-full ">
+      <InputField
+        title="বিভাগ "
+        type={"text"}
+        placeholder={"বিভাগের নাম লিখুন"}
+        // setValue={setStudentName}
+      />
+      {/* ==== Submit button ==== */}
+      <div className="flex gap-[20px] justify-end mt-[30px] mb-[10px]">
+        <button className="flex items-center gap-[8px] bg-[#008000]  text-white px-[20px] py-[8px] rounded-[8px]">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BivhagCreateUpdate;
