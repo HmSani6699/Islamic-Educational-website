4505272bce6826e52df7cc793769410960831570
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-02-15 09:02:08 +0600

    complete three page design in admin panale

--- app/Dashboard/Admin/AcademicSetup/AddCategoy/AddCategory.jsx
+++ app/Dashboard/Admin/AcademicSetup/AddCategoy/AddCategory.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaArrowLeft, FaPlus } from "react-icons/fa";
+import CategoryCreateUpdate from "./CategoryCreateUpdate";
+import CategoryList from "./CategoryList";
 
 const AddCategory = () => {
-  return <div>hallo category</div>;
+  const [openForm, setOpenForm] = useState("categoryList");
+
+  return (
+    <div>
+      <div className="flex items-center justify-between  px-[20px] py-[10px] bg-white mt-[40px] rounded-t-[20px] border-t-[2px] border-[#008000]">
+        <div>
+          {openForm === "categoryList" ? (
+            <h2 className="text-[20px] font-semibold">মোট শ্রেণী (20)</h2>
+          ) : (
+            <h2
+              onClick={() => setOpenForm("categoryList")}
+              className="text-[20px] font-semibold py-[6px] flex items-center gap-[10px] cursor-pointer"
+            >
+              <FaArrowLeft />
+              {openForm === "categoryList" ? "নতুন শ্রেণী" : "নতুন শ্রেণী"}
+            </h2>
+          )}
+        </div>
+        {openForm === "categoryList" && (
+          <button
+            onClick={() => setOpenForm("bivhagCreateUpdate")}
+            className="flex items-center gap-[8px] bg-[#008000]  text-white px-[20px] py-[10px] rounded-[8px]"
+          >
+            <FaPlus />
+            নতুন শ্রেণী
+          </button>
+        )}
+      </div>
+      {/* =====> Main content  <===== */}
+      <div>
+        {openForm === "bivhagCreateUpdate" ? (
+          <CategoryCreateUpdate />
+        ) : (
+          <CategoryList />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AddCategory;
--- app/Dashboard/Admin/AcademicSetup/AddCategoy/CategoryList.jsx
+++ app/Dashboard/Admin/AcademicSetup/AddCategoy/CategoryList.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import CustomSearchForm from "../../../../Component/CustomSearchForm/CustomSearchForm";
+import update_green_icon from "../../../../../public/svg/updateGreenIcon.svg";
+import delete_icon from "../../../../../public/svg/delete_icon.svg";
 
 const CategoryList = () => {
-  return <div>list</div>;
+  return (
+    <div className="w-full overflow-x-auto bg-white py-5 px-2.5 rounded-[10px] mt-10">
+      {/* =====> Search Div  <====== */}
+      <CustomSearchForm />
+
+      {/* =====> Admission list table <====== */}
+      <table
+        className="w-full mt-5 text-left border-collapse w-overflow-x-auto table-auto "
+        cellSpacing="0"
+      >
+        <tbody>
+          <tr className="bg-[#F1F2F3] font-semibold">
+            <th
+              scope="col"
+              className="h-10 ps-2.5 text-[#2E3138] text-[16px] font-[500]"
+            >
+              ID
+            </th>
+            <th
+              scope="col"
+              className="h-10 px-0 text-[#2E3138] text-[16px] font-[500] "
+            >
+              Name
+            </th>
+
+            <th
+              scope="col"
+              className="h-10 px-0 text-[#2E3138] text-[16px] font-[500] text-center"
+            >
+              Action
+            </th>
+          </tr>
+
+          <tr>
+            <td className="h-10 py-2 pl-[10px]">1</td>
+            <td className="h-10 py-2">Md Korim </td>
+
+            <td className="h-10 py-2 mt-[10px] flex justify-center">
+              <div className="flex items-center gap-[10px]">
+                <button>
+                  <img src={update_green_icon} alt="" />
+                </button>
+
+                <button>
+                  <img src={delete_icon} alt="" />
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td className="h-10 py-2 pl-[10px]">1</td>
+            <td className="h-10 py-2">Md Sadiq</td>
+
+            <td className="h-10 py-2 mt-[10px] flex justify-center">
+              <div className="flex items-center gap-[10px]">
+                <button>
+                  <img src={update_green_icon} alt="" />
+                </button>
+
+                <button>
+                  <img src={delete_icon} alt="" />
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td className="h-10 py-2 pl-[10px]">1</td>
+            <td className="h-10 py-2">Md Norol Amin</td>
+
+            <td className="h-10 py-2 mt-[10px] flex justify-center">
+              <div className="flex items-center gap-[10px]">
+                <button>
+                  <img src={update_green_icon} alt="" />
+                </button>
+
+                <button>
+                  <img src={delete_icon} alt="" />
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CategoryList;
