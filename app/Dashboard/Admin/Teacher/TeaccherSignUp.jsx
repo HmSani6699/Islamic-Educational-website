e1a8d4657af728caf1abd9b3bc7ca32e8579dcec
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-02-17 05:42:32 +0600

    added update code in admin panale

--- app/Dashboard/Admin/Teacher/TeaccherSignUp.jsx
+++ app/Dashboard/Admin/Teacher/TeaccherSignUp.jsx
@@ -4,20 +4,22 @@ import { GrLinkNext } from "react-icons/gr";
 
 const TeaccherSignUp = ({ setPage }) => {
   return (
-    <div className="bg-white p-[20px] rounded-[10px] mt-10 w-[500px]">
+    <div className="bg-white p-[20px] rounded-[10px] mt-10 w-full">
       <h2 className="text-[22px]  font-semibold text-center mt-[8px]">
         শিক্ষক অ্যাকাউন্ট
       </h2>
 
       {/* =====>  Create acount form <===== */}
       <div>
-        <div className="mt-[50px] flex flex-col gap-[25px]">
-          <InputField
-            title="শিক্ষকের নাম"
-            type={"text"}
-            placeholder={"আপনার শিক্ষকের নাম লিখুন"}
-            setValue={""}
-          />
+        <div className="mt-[50px] grid grid-cols-1 lg:grid-cols-2 gap-[25px]">
+          <div className="w-full col-span-2">
+            <InputField
+              title="শিক্ষকের নাম"
+              type={"text"}
+              placeholder={"আপনার শিক্ষকের নাম লিখুন"}
+              setValue={""}
+            />
+          </div>
           <InputField
             title="শিক্ষকের ইমেল"
             type={"text"}
--- app/Dashboard/Admin/Teacher/Teacher.jsx
+++ app/Dashboard/Admin/Teacher/Teacher.jsx
@@ -13,39 +13,23 @@ const Teacher = () => {
     <div>
       {/* ======> Top part <==== */}
       <div
-        className={`${
-          createTechercurrentForm !== "form_success" &&
-          "bg-white border-t-[2px] border-[#008000]"
-        } flex items-center justify-between  px-[20px] py-[10px]  mt-[40px] rounded-t-[20px] `}
+        className={`${currentPage === "teacherList" ? "block" : "hidden"}
+        bg-white border-t-[2px] border-[#008000]
+         flex items-center justify-between  px-[20px] py-[10px]  mt-[40px] rounded-t-[20px] `}
       >
-        <div
-          className={`${
-            createTechercurrentForm === "form_success" ? "hidden" : "block"
-          }`}
-        >
-          {currentPage === "teacherList" ? (
-            <h2 className="text-[20px] font-semibold">Teacher List</h2>
-          ) : (
-            <h2
-              onClick={() => setCurrentPage("teacherList")}
-              className="text-[20px] font-semibold py-[6px] flex items-center gap-[10px] cursor-pointer"
-            >
-              <FaArrowLeft />
-              {currentPage === "teacherList" ? "Teacher Create" : "Add Class"}
-            </h2>
-          )}
+        <div>
+          <h2 className="text-[20px] font-semibold">Teacher List</h2>
         </div>
 
-        {currentPage === "teacherList" && (
-          <button
-            onClick={() => setCurrentPage("teacherCreateUpdate")}
-            className="flex items-center gap-[8px] bg-[#008000]  text-white px-[20px] py-[10px] rounded-[8px]"
-          >
-            <FaPlus />
-            New Teacher
-          </button>
-        )}
+        <button
+          onClick={() => setCurrentPage("teacherCreateUpdate")}
+          className="flex items-center gap-[8px] bg-[#008000]  text-white px-[20px] py-[10px] rounded-[8px]"
+        >
+          <FaPlus />
+          New Teacher
+        </button>
       </div>
+
       {currentPage === "teacherList" ? (
         <TeacherList setPage={setCurrentPage} />
       ) : currentPage === "teacherCreateUpdate" ? (
