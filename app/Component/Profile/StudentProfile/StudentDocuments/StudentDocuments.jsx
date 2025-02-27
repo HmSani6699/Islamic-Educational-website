980adbeb52ac2493aaeeab025e7f92b039df0965
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-02-27 09:53:15 +0600

    added document

--- app/Component/Profile/StudentProfile/StudentDocuments/StudentDocuments.jsx
+++ app/Component/Profile/StudentProfile/StudentDocuments/StudentDocuments.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { HiArrowDownTray } from "react-icons/hi2";
 
 const StudentDocuments = () => {
-  return <div>document</div>;
+  return (
+    <div className="card_shadow mt-[30px]">
+      <h2 className="text-[#202C4B] text-[16px] font-[500] px-[20px] py-[15px] border-b">
+        Document
+      </h2>
+
+      <div className="flex items-center justify-between border bg-[#f2f5ffa9] mx-[20px] p-[15px] rounded-[5px] my-[20px]">
+        <div className="flex items-center gap-[10px]">
+          <div className=" rounded-[5px] bg-white text-[10px] p-[5px] font-semibold">
+            PDF
+          </div>
+          <h2 className="text-[#202C4B] text-[14px] font-semibold">
+            BirthCertificate.pdf
+          </h2>
+        </div>
+        <button className="bg-[#506EE4] text-white p-[5px] rounded-[5px]">
+          <HiArrowDownTray />
+        </button>
+      </div>
+      <div className="flex items-center justify-between border bg-[#f2f5ffa9] mx-[20px] p-[15px] rounded-[5px] my-[20px]">
+        <div className="flex items-center gap-[10px]">
+          <div className=" rounded-[5px] bg-white text-[10px] p-[5px] font-semibold ">
+            PDF
+          </div>
+          <h2 className="text-[#202C4B] text-[14px] font-semibold">
+            BirthCertificate.pdf
+          </h2>
+        </div>
+        <button className="bg-[#506EE4] text-white p-[5px] rounded-[5px]">
+          <HiArrowDownTray />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default StudentDocuments;
