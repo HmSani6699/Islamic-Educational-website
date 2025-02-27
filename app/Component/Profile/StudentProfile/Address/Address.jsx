5e3ce19c461ddbd9d971ac18057dab804b56de30
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-02-27 19:51:51 +0600

    added others page

--- app/Component/Profile/StudentProfile/Address/Address.jsx
+++ app/Component/Profile/StudentProfile/Address/Address.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { MdOutlineAddLocationAlt, MdOutlineLocationOn } from "react-icons/md";
 
 const Address = () => {
-  return <div>address</div>;
+  return (
+    <div className="card_shadow mt-[30px]">
+      <h2 className="text-[#202C4B] text-[16px] font-[500] px-[20px] py-[15px] border-b">
+        Address
+      </h2>
+
+      <div className="p-[10px]">
+        <div className="p-[10px] rounded-[5px]  ">
+          <div className="flex items-center gap-[10px]">
+            <div className=" rounded-[5px] bg-white text-[16px] p-[5px] font-semibold">
+              <MdOutlineAddLocationAlt className="text-gray-500" />
+            </div>
+            <div>
+              <h2 className="text-[#202C4B] text-[14px] font-semibold">
+                Current Address
+              </h2>
+              <p className="text-[14px] text-gray-500 mt-[4px]">
+                3495 Red Hawk Road, Buffalo Lake, MN 55314
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="p-[10px] rounded-[5px] ">
+          <div className="flex items-center gap-[10px]">
+            <div className=" rounded-[5px] bg-white text-[16px] p-[5px] font-semibold">
+              <MdOutlineLocationOn className="text-gray-500" />
+            </div>
+            <div>
+              <h2 className="text-[#202C4B] text-[14px] font-semibold">
+                Current Address
+              </h2>
+              <p className="text-[14px] text-gray-500 mt-[4px]">
+                3495 Red Hawk Road, Buffalo Lake, MN 55314
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Address;
--- app/Component/Profile/StudentProfile/PreviousOrganaization/PreviousOrganaization.jsx
+++ app/Component/Profile/StudentProfile/PreviousOrganaization/PreviousOrganaization.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const PreviousOrganaization = () => {
-  return <div>organaization</div>;
+  return (
+    <div className="card_shadow mt-[30px]">
+      <h2 className="text-[#202C4B] text-[16px] font-[500] px-[20px] py-[15px] border-b">
+        Previous Organaization Details
+      </h2>
+
+      <div className="rounded-[5px] p-[20px] flex items-center justify-between ">
+        <div>
+          <h2 className="text-[#202C4B] text-[14px] font-semibold">
+            Previous Organaization Name
+          </h2>
+          <p className="text-[14px] text-gray-500 mt-[4px]">
+            Pakunda Islamia Madrasa
+          </p>
+        </div>
+        <div>
+          <h2 className="text-[#202C4B] text-[14px] font-semibold">
+            Organaization Address
+          </h2>
+          <p className="text-[14px] text-gray-500 mt-[4px]">
+            Pakunda, Sonargon, Narayoungonj
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PreviousOrganaization;
